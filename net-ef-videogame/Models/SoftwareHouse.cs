75aff6912ec5b76dfd157389f798521e454579e9
Brody14 <stefania.spinas@gmail.com>
2023-09-22 15:01:56 +0200

    FEAT models and migration

---
+++ net-ef-videogame/Models/SoftwareHouse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame.Models
+{
+    public class SoftwareHouse
+    {
+
+        public int SoftwareHouseId { get; set; }
+        public string Name { get; set; }
+        public string VatNumber {  get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Videogame> Videogames {  get; set; }
+
+        public override string ToString()
+        {
+            return @$"
+Id: {SoftwareHouseId}
+Nome: {Name},
+Partita Iva: {VatNumber}
+Città: {City}
+Nazione: {Country}";        }
+    }
+}
---
+++ net-ef-videogame/Models/Videogame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame.Models
+{
+    public class Videogame
+    {
+        public long VideogameId { get; set; }
+        public string Name { get; set; }
+        public string Overview { get; set; }
+        public DateTime Release_date { get; set; }
+
+        public int SoftwareHouseId { get; set; }
+        public SoftwareHouse? SoftwareHouse { get;  set; }
+
+        public override string ToString()
+        {
+            string result = $@"
+ID: {VideogameId} 
+Nome videogame: {Name}
+Trama: {Overview}
+Data di rilascio: {Release_date.ToString("dd/MM/yyyy")}";
+
+            if(SoftwareHouse != null)
+            {
+                result += $@"
+ID: {VideogameId} 
+Nome videogame: {Name}
+Trama: {Overview}
+Data di rilascio: {Release_date.ToString("dd/MM/yyyy")}
+Software House: {SoftwareHouse.Name}";
+            }
+
+            return result;
+        }
+    }
+}
+
